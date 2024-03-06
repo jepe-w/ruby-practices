7660057ee568296bc8b1b38c82d2a9db6c670761
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-06 10:32:50 +0900

    レビュー指摘箇所の改善

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,67 +1,56 @@
 #!/usr/bin/env ruby
 require "date"
 require "optparse"
+require 'debug'
 
-class WorkClendar
+class Clendar
   def initialize()
-  opt = OptionParser.new
+    opt = OptionParser.new
 
-  opt.on('-y') {|v| v}
-  opt.on('-m') {|v| v}
-
-  @request = opt.parse!(ARGV)
-  @request = ARGV
+    opt.on('-y') 
+    opt.on('-m') 
+    opt.parse!(ARGV)
+    ARGV
   end
   
-  def calendar
-    days = []
-    num = 0
-
-    #デフォルトの日にちを取得
-    default = Date.today.to_s
-    default = default.split("-")
-    if @request == []
-      default_year = default[0]
+  def create_calendar
+    if ARGV == []
+      target_year = Date.today.year
     else
-      default_year = @request[0]
+      target_year = ARGV[0]
     end
     
-    if @request == []
-      default_month = default[1]
+    if ARGV == []
+      target_month = Date.today.mon
     else
-      default_month = @request[1]
+      target_month = ARGV[1]
     end
-
-    #日にちを取得
-    designated_date = Date.new(default_year.to_i, default_month.to_i, 1)
-    month_days = ((designated_date >> 1) - 1).day
-    date = designated_date.wday
+    
+    first_day_of_designated_date = Date.new(target_year.to_i, target_month.to_i, 1)
+    month_days = Date.new(target_year.to_i, target_month.to_i, -1).day
+    get_day_of_week = first_day_of_designated_date.wday
     
     #曜日ぶんから文字を挿入（初日のスタート位置（曜日）をとるため）
-    date.times do
+    days = []
+    get_day_of_week.times do
       days.push("  ")
     end
     #その月の日数分daysに日にちを追加
-    month_days.times do
-      num += 1
-      if num < 10
-        days.push(" #{num}")
+    month_days.times do |n|
+      n += 1
+      if n < 10
+        days.push(" #{n}")
       else
-        days.push(num)
+        days.push(n)
       end
     end
 
-    #余白を含めた日にちを一週間ごとに分割
-    new_days = days.each_slice(7).to_a
-    
-    #カレンダーの年、月を出力
-    puts designated_date.strftime("%B %Y").center(20)
-
-    #一週間ごとに分けた配列をそれぞれくっつけて曜日と一緒に出力。
+    week = days.each_slice(7).to_a
+    puts first_day_of_designated_date.strftime("%B %Y").center(20)
     puts ("日 月 火 水 木 金 土")
-    new_days = new_days.map{|item| puts item.join(" ")}
+    week.each{|day| puts day.join(" ")}
   end
 end
 
-result_calendar = WorkClendar.new
-result_calendar.calendar
+result_calendar = Clendar.new
+result_calendar.create_calendar
