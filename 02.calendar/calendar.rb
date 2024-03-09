adb31b67610a0ade0dee14d570b92a5c59fd248d
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-09 20:57:21 +0900

    レビューでの指摘箇所改善　v2

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,56 +1,44 @@
 #!/usr/bin/env ruby
 require "date"
 require "optparse"
-require 'debug'
 
-class Clendar
-  def initialize()
-    opt = OptionParser.new
+def create_calendar
+  opt = OptionParser.new
+  target_year = Date.today.year
+  target_month = Date.today.mon
+  option = {}
 
-    opt.on('-y') 
-    opt.on('-m') 
-    opt.parse!(ARGV)
-    ARGV
-  end
+  opt.on('-y Integer') { 
+    |y| option[:y] = y
+    target_year = option[:y]
+  }
+  opt.on('-m Integer') { 
+    |m| option[:m] = m
+    target_month = option[:m]}
+  opt.parse!(ARGV)
   
-  def create_calendar
-    if ARGV == []
-      target_year = Date.today.year
-    else
-      target_year = ARGV[0]
-    end
-    
-    if ARGV == []
-      target_month = Date.today.mon
-    else
-      target_month = ARGV[1]
-    end
-    
-    first_day_of_designated_date = Date.new(target_year.to_i, target_month.to_i, 1)
-    month_days = Date.new(target_year.to_i, target_month.to_i, -1).day
-    get_day_of_week = first_day_of_designated_date.wday
-    
-    #曜日ぶんから文字を挿入（初日のスタート位置（曜日）をとるため）
-    days = []
-    get_day_of_week.times do
-      days.push("  ")
-    end
-    #その月の日数分daysに日にちを追加
-    month_days.times do |n|
-      n += 1
-      if n < 10
-        days.push(" #{n}")
-      else
-        days.push(n)
-      end
-    end
-
-    week = days.each_slice(7).to_a
-    puts first_day_of_designated_date.strftime("%B %Y").center(20)
-    puts ("日 月 火 水 木 金 土")
-    week.each{|day| puts day.join(" ")}
+  first_day = Date.new(target_year.to_i, target_month.to_i, 1)
+  month_day = Date.new(target_year.to_i, target_month.to_i, -1).day
+  day_of_week = first_day.wday
+  
+  #曜日ぶんから文字を挿入（初日のスタート位置（曜日）をとるため）
+  days = []
+  day_of_week.times do
+    days.push("  ")
   end
+  #その月の日数分daysに日にちを追加
+  1.upto(month_day) do |d|
+    if d < 10
+      days.push(" #{d}")
+    else
+      days.push(d)
+    end
+  end
+
+  week = days.each_slice(7).to_a
+  puts first_day.strftime("%B %Y").center(20)
+  puts ("日 月 火 水 木 金 土")
+  week.each{ |day| puts day.join(" ") }
 end
 
-result_calendar = Clendar.new
-result_calendar.create_calendar
+create_calendar
