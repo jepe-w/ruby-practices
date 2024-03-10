871a54a606c712784674977481e9875c35672691
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-10 13:21:39 +0900

    レビューでの指摘箇所改善　v1

--- 03.bowling/y-bowling.rb
+++ 03.bowling/y-bowling.rb
@@ -5,57 +5,48 @@ score = score.split(',')
 shots = []
 
 score.each do |s|
-  #最後の一回は１０を一回分ずつ追加
   if shots.length < 18
-    if s == 'x'
+    if s == 'X'
       shots << 10
       shots << 0
     else
       shots << s.to_i
     end
-
   else
-    if s == 'x'
-      shots << 10
-    else
-      shots << s.to_i
-    end
-
+    shots << if s == 'X'
+               10
+             else
+               s.to_i
+             end
   end
-
 end
 
-#最後の10回目は場合によっては３回になる
 frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
-#２回ずつで分けて最後に一つ残った場合は１０回目に追加する
+
 if frames.length == 11
   frames[9] += frames[10]
   frames.pop
 end
 
-
 point = 0
 count = 0
- #ストライクの場合は次のframeの合計を加算
- #スペアの場合は次のframeの一回目の点数を加算
 frames.each do |frame|
-  if frame.length == 2
-    if frame[0] == 10
-      point += frame.sum + frames[count + 1][0] + frames[count + 1][1]
-    elsif frame.sum == 10 
-      point += frame.sum + frames[count + 1][0]
-    else
-    point += frame.sum
-    end
-
-  else
-    point += frame.sum
-  end
+  point += if frame.length == 2 && frame[0] == 10
+             if frames[count + 1].length == 3
+               10 + frames[count + 1][0] + frames[count + 1][1]
+             elsif frames[count + 1][0] == 10
+               10 + 10 + frames[count + 2][0]
+             else
+               10 + frames[count + 1][0] + frames[count + 1][1]
+             end
+           elsif frame.length == 2 && frame.sum == 10
+             frame.sum + frames[count + 1][0]
+           else
+             frame.sum
+           end
   count += 1
 end
-
-
-p point
+puts point
