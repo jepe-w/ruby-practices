69c20ec7f0e9311c298bcae39d81d23a5f4f690d
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-13 19:46:39 +0900

    レビュー指摘箇所の改善　v2

--- 03.bowling/y-bowling.rb
+++ 03.bowling/y-bowling.rb
@@ -1,53 +1,35 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-split_score = score.split(',')
+scores = ARGV[0].split(',')
 shots = []
 
-split_score.each do |s|
-  if shots.length < 18
-    if s == 'X'
-      shots << 10
-      shots << 0
-    else
-      shots << s.to_i
-    end
-  else
-    shots << if s == 'X'
-               10
-             else
-               s.to_i
-             end
-  end
+scores.each do |s|
+  shots << (s == 'X' ? 10 : s.to_i)
+  shots << 0 if shots.length < 18 && s == 'X'
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 frames[9] += frames.pop if frames.length == 11
-
-point = 0
-frames.each_with_index do |frame, i|
+point = frames.each_with_index.sum do |frame, i|
   next_frame = frames[i + 1]
-  not_last_frame = frame.length == 2
+  not_last_frame = i != 9
   strilke = frame[0] == 10
   spare = frame.sum == 10
 
-  point += if not_last_frame && strilke
-             if next_frame.length == 3
-               10 + next_frame[0..1].sum
-             elsif next_frame[0] == 10
-               10 + 10 + frames[i + 2][0]
-             else
-               10 + next_frame.sum
-             end
-           elsif not_last_frame && spare
-             frame.sum + next_frame[0]
-           else
-             frame.sum
-           end
+  if not_last_frame && strilke
+    if next_frame.length == 3
+      10 + next_frame[0..1].sum
+    elsif next_frame[0] == 10
+      10 + 10 + frames[i + 2][0]
+    else
+      10 + next_frame.sum
+    end
+  elsif not_last_frame && spare
+    frame.sum + next_frame[0]
+  else
+    frame.sum
+  end
 end
 puts point
