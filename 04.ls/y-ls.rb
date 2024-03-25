dc504ed408be71b9b61926efc33fccafe081715b
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-25 16:39:48 +0900

    条件分岐を後置ifに変更、正規表現修正、商余りをdivmodに変更、繰り上げの計算をfloatを使わない仕様に変更

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -4,7 +4,7 @@
 def title_chars_count
   word_count = []
   Dir.foreach('.') do |item|
-    next if item == '.' || item =~ /^\..*/
+    next if /\A\..*/.match?(item)
 
     word_count.push(item.length)
   end
@@ -17,11 +17,9 @@ max_char_number = word_count.max
 def directories_and_filles_get(max_char_number)
   directories_and_folder_list = []
   Dir.foreach('.') do |item|
-    next if item == '.' || item =~ /^\..*/
+    next if /\A\..*/.match?(item)
 
-    number_of_blank = max_char_number - item.length
-    blank = ' ' * number_of_blank
-    output_title = item + blank
+    output_title = item.ljust(max_char_number)
     directories_and_folder_list.push(output_title)
   end
   directories_and_folder_list
@@ -29,13 +27,13 @@ end
 
 element_count_number = directories_and_filles_get(max_char_number).length
 sort_directories_and_folder_list = directories_and_filles_get(max_char_number).sort
-float_elms = element_count_number.to_f
 
-def output_directories(row_num, sort_directories_and_folder_list, float_elms, element_count_number)
+def output_directories(row_num, sort_directories_and_folder_list, element_count_number)
   output_arrays = []
-  exact_num = (float_elms / row_num).ceil
-  surplus_num = element_count_number % row_num
-  column_num = element_count_number / row_num
+  acolumn_and_surplus_num = element_count_number.divmod(row_num)
+  surplus_num = acolumn_and_surplus_num[1]
+  column_num = acolumn_and_surplus_num[0]
+  exact_num = surplus_num + 1 unless acolumn_and_surplus_num[1].zero?
 
   exact_num.times do
     output_arrays.push([])
@@ -45,11 +43,9 @@ def output_directories(row_num, sort_directories_and_folder_list, float_elms, el
     exact_num_count = i % exact_num
     column_num_count = i % column_num
 
-    if surplus_num.zero?
-      output_arrays[column_num_count].push(v)
-    else
-      output_arrays[exact_num_count].push(v)
-    end
+    next output_arrays[column_num_count].push(v) if surplus_num.zero?
+
+    output_arrays[exact_num_count].push(v)
   end
   output_arrays.each { |item| puts item.join('  ') }
 end
@@ -63,8 +59,5 @@ end
 
 ROW_NUM = 3
 
-if word_count.length < ROW_NUM
-  output_directories_one_row(sort_directories_and_folder_list)
-else
-  output_directories(ROW_NUM, sort_directories_and_folder_list, float_elms, element_count_number)
-end
+output_directories_one_row(sort_directories_and_folder_list) if word_count.length < ROW_NUM
+output_directories(ROW_NUM, sort_directories_and_folder_list, element_count_number) unless word_count.length < ROW_NUM
