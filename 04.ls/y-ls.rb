0915f00c83a6c6cb915a5b3496842debb9a05d1c
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-26 11:45:59 +0900

    title_chars_count,directories_and_filles_getをselect,mapを使って簡素化。不要になったcolumn_numを削除。output_arrays = []no

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -2,48 +2,28 @@
 # frozen_string_literal: true
 
 def title_chars_count
-  word_count = []
-  Dir.foreach('.') do |item|
-    next if /\A\..*/.match?(item)
+  Dir.foreach('.').select { !/\A\..*/.match?(_1) }.map { _1.length }
 
-    word_count.push(item.length)
-  end
-  word_count
 end
 
 word_count = title_chars_count
 max_char_number = word_count.max
 
 def directories_and_filles_get(max_char_number)
-  directories_and_folder_list = []
-  Dir.foreach('.') do |item|
-    next if /\A\..*/.match?(item)
+  Dir.foreach('.').select { !/\A\..*/.match?(_1) }.map { _1.ljust(max_char_number) }
 
-    output_title = item.ljust(max_char_number)
-    directories_and_folder_list.push(output_title)
-  end
-  directories_and_folder_list
 end
 
 element_count_number = directories_and_filles_get(max_char_number).length
 sort_directories_and_folder_list = directories_and_filles_get(max_char_number).sort
 
 def output_directories(row_num, sort_directories_and_folder_list, element_count_number)
-  output_arrays = []
-  acolumn_and_surplus_num = element_count_number.divmod(row_num)
-  surplus_num = acolumn_and_surplus_num[1]
-  column_num = acolumn_and_surplus_num[0]
-  exact_num = surplus_num + 1 unless acolumn_and_surplus_num[1].zero?
-
-  exact_num.times do
-    output_arrays.push([])
-  end
+  _, surplus_num = element_count_number.divmod(row_num)
+  exact_num = surplus_num + 1 unless surplus_num.zero?
+  output_arrays = exact_num.times.map { [] }
 
   sort_directories_and_folder_list.each_with_index do |v, i|
-    exact_num_count = i % exact_num
-    column_num_count = i % column_num
-
-    next output_arrays[column_num_count].push(v) if surplus_num.zero?
+    _, exact_num_count = i.divmod(exact_num)
 
     output_arrays[exact_num_count].push(v)
   end
