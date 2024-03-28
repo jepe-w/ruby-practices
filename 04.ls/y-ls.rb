f492eafe082e7260c7d6d34a4798cbfeb1fe2dcb
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-28 11:28:24 +0900

    ROW_NUMに渡された数字の列になるように修正。コード重複箇所を改善。変数の調整

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -1,36 +1,32 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def title_chars_count
-  Dir.foreach('.').reject { /\A\..*/.match?(_1) }.map(&:length)
+def contents_checker
+  Dir.foreach('.').reject { _1.start_with?('.') }
 end
 
-word_count = title_chars_count
-max_char_number = word_count.max
-
-def contents_get(max_char_number)
-  Dir.foreach('.').reject { /\A\..*/.match?(_1) }.map { _1.ljust(max_char_number) }
-end
-
-element_count_number = contents_get(max_char_number).length
-sort_contents = contents_get(max_char_number).sort
+title_chars_count = contents_checker.map(&:length)
+max_char_number = title_chars_count.max
+contents_get = contents_checker.map { _1.ljust(max_char_number) }
+element_count_number = contents_get.length
+sort_contents = contents_get.sort
+ROW_NUM = 3
 
 def output_directories(row_num, sort_contents, element_count_number)
-  if element_count_number < ROW_NUM
+  if element_count_number <= row_num
     puts sort_contents.join('  ')
   else
-    surplus_num = element_count_number % row_num
-    rows_num_to_make = surplus_num + 1 unless surplus_num.zero?
+    column_num, surplus_num = element_count_number.divmod(row_num)
+    rows_num_to_make = column_num + 1 unless surplus_num.zero?
     output_arrays = Array.new(rows_num_to_make) { [] }
+
     sort_contents.each_with_index do |v, i|
       rows_num_to_push = i % rows_num_to_make
-
       output_arrays[rows_num_to_push].push(v)
     end
+
     output_arrays.each { |item| puts item.join('  ') }
   end
 end
-
-ROW_NUM = 3
 
 output_directories(ROW_NUM, sort_contents, element_count_number)
