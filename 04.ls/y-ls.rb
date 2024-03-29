1bd562ec1b09ef9996eaee4160bcb9278f161b57
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-29 16:45:12 +0900

    日本語表記ファイルgaレイアウト崩れの無い様に改善

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -5,9 +5,12 @@ def contents_checker
   Dir.foreach('.').reject { _1.start_with?('.') }
 end
 
-title_chars_count = contents_checker.map(&:length)
+title_chars_count = contents_checker.map { |item| item.encode('EUC-JP').bytesize }
 max_char_number = title_chars_count.max
-contents_get = contents_checker.map { _1.ljust(max_char_number) }
+contents_get = contents_checker.map do |item|
+  brunk_char_number = max_char_number - item.encode('EUC-JP').bytesize
+  item + ' ' * brunk_char_number
+end
 element_count_number = contents_get.length
 sort_contents = contents_get.sort
 ROW_NUM = 3
