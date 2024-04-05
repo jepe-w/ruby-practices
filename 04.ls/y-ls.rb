57a8c373e73b30debb64cc0090cd886e1de3a32c
jepe-w <yoshino_k@vitallead.co.jp>
2024-04-05 11:05:50 +0900

    不要な代入を削除、sortをファイル取得時に実行、変数の調整等

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -2,31 +2,23 @@
 # frozen_string_literal: true
 
 def filenames
-  Dir.foreach('.').reject { _1.start_with?('.') }
+  Dir.foreach('.').reject { _1.start_with?('.') }.sort
 end
 
-title_chars_count = filenames.map(&:length)
-max_char_count = title_chars_count.max
-filename_processed_for_output = filenames.map { _1.ljust(max_char_count) }
-element_count = filename_processed_for_output.length
-sort_filename = filename_processed_for_output.sort
+max_char_count = filenames.map(&:length).max
+sorted_filenames = filenames
 ROW_NUM = 3
 
-def output_directories(row_num, sort_filename, element_count)
-  if element_count <= row_num
-    puts sort_filename.join('  ')
-  else
-    rows_num_to_make, surplus_num = element_count.divmod(row_num)
-    rows_num_to_make += 1 unless surplus_num.zero?
-    output_arrays = Array.new(rows_num_to_make) { [] }
+def output_filenames(row_num, sorted_filenames, max_char_count)
+  filenames_length = sorted_filenames.length
+  rows_num_to_make = filenames_length.ceildiv(row_num)
+  output_arrays = Array.new(rows_num_to_make) { [] }
 
-    sort_filename.each_with_index do |v, i|
-      rows_num_to_push = i % rows_num_to_make
-      output_arrays[rows_num_to_push].push(v)
-    end
-
-    output_arrays.each { |item| puts item.join('  ') }
+  sorted_filenames.each_with_index do |v, i|
+    rows_num_to_push = i % rows_num_to_make
+    output_arrays[rows_num_to_push].push(v.ljust(max_char_count))
   end
+  output_arrays.each { puts _1.join('  ') }
 end
 
-output_directories(ROW_NUM, sort_filename, element_count)
+output_filenames(ROW_NUM, sorted_filenames, max_char_count)
