f3f9c679e42527de7706c8e536a12275cb4228ac
jepe-w <yoshino_k@vitallead.co.jp>
2024-04-07 15:24:50 +0900

    変数の調整、不要な処理の記載を削除

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -5,20 +5,18 @@ def filenames
   Dir.foreach('.').reject { _1.start_with?('.') }.sort
 end
 
-max_char_count = filenames.map(&:length).max
-sorted_filenames = filenames
+max_char_num = filenames.map(&:length).max
 ROW_NUM = 3
 
-def output_filenames(row_num, sorted_filenames, max_char_count)
-  filenames_length = sorted_filenames.length
-  rows_num_to_make = filenames_length.ceildiv(row_num)
-  output_arrays = Array.new(rows_num_to_make) { [] }
+def output_filenames(row_num, filenames, max_char_num)
+  column_num = filenames.length.ceildiv(row_num)
+  matrix_to_display = Array.new(column_num) { [] }
 
-  sorted_filenames.each_with_index do |v, i|
-    rows_num_to_push = i % rows_num_to_make
-    output_arrays[rows_num_to_push].push(v.ljust(max_char_count))
+  filenames.each_with_index do |v, i|
+    rows_push_num = i % column_num
+    matrix_to_display[rows_push_num].push(v.ljust(max_char_num))
   end
-  output_arrays.each { puts _1.join('  ') }
+  matrix_to_display.each { puts _1.join('  ') }
 end
 
-output_filenames(ROW_NUM, sorted_filenames, max_char_count)
+output_filenames(ROW_NUM, filenames, max_char_num)
