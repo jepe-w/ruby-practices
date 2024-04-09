2e90e3c0803a45dd4b6407f34428e8e7872f44a4
jepe-w <yoshino_k@vitallead.co.jp>
2024-04-09 09:04:38 +0900

    変数調整、不要にも考えられる引数受け取りを取り消し(max_char_num)

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -5,18 +5,17 @@ def filenames
   Dir.foreach('.').reject { _1.start_with?('.') }.sort
 end
 
-max_char_num = filenames.map(&:length).max
-ROW_NUM = 3
-
-def output_filenames(row_num, filenames, max_char_num)
-  column_num = filenames.length.ceildiv(row_num)
-  matrix_to_display = Array.new(column_num) { [] }
-
+def output_filenames(row_length, filenames)
+  max_filename_length = filenames.map(&:length).max
+  column_length = filenames.length.ceildiv(row_length)
+  matrix_to_display = Array.new(column_length) { [] }
+  
   filenames.each_with_index do |v, i|
-    rows_push_num = i % column_num
-    matrix_to_display[rows_push_num].push(v.ljust(max_char_num))
+    row_index = i % column_length
+    matrix_to_display[row_index].push(v.ljust(max_filename_length))
   end
   matrix_to_display.each { puts _1.join('  ') }
 end
 
-output_filenames(ROW_NUM, filenames, max_char_num)
+ROW_LENGTH = 3
+output_filenames(ROW_LENGTH, filenames)
