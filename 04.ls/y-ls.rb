05bb6d65a156ac5dbb265684b8bfaeaa2e0fb7c6
jepe-w <yoshino_k@vitallead.co.jp>
2024-04-10 10:57:31 +0900

    -aオプション作成。実際のlsコマンドに近づけるためにソートを修正

--- 04.ls/y-ls.rb
+++ 04.ls/y-ls.rb
@@ -1,21 +1,34 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def filenames
-  Dir.foreach('.').reject { _1.start_with?('.') }.sort
+require 'optparse'
+
+def option
+  ARGV.getopts('a')
 end
 
-def output_filenames(row_length, filenames)
-  max_filename_length = filenames.map(&:length).max
-  column_length = filenames.length.ceildiv(row_length)
+def filenames(option_item)
+  if option_item['a']
+    Dir.foreach('.').sort_by { |item| item.downcase.delete('.') }
+  else
+    Dir.foreach('.').reject { _1.start_with?('.') }.sort_by { |item| item.downcase.delete('.') }
+  end
+end
+
+def output_filenames(row_length, sorted_filenames)
+  max_filename_length = sorted_filenames.map(&:length).max
+  column_length = sorted_filenames.length.ceildiv(row_length)
   matrix_to_display = Array.new(column_length) { [] }
-  
-  filenames.each_with_index do |v, i|
+
+  sorted_filenames.each_with_index do |v, i|
     row_index = i % column_length
     matrix_to_display[row_index].push(v.ljust(max_filename_length))
   end
   matrix_to_display.each { puts _1.join('  ') }
 end
 
+option_item = option
+sorted_filenames = filenames(option_item)
 ROW_LENGTH = 3
-output_filenames(ROW_LENGTH, filenames)
+
+output_filenames(ROW_LENGTH, sorted_filenames)
