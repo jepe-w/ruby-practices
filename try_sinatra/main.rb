787378ec11f37c5b2f3072064de29c71d7cf3e75
jepe-w <yoshino_k@vitallead.co.jp>
2024-04-18 09:37:14 +0900

    jsonファイルの作成

--- try_sinatra/main.rb
+++ try_sinatra/main.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
-# def set_memos(file_path, memos)
-#   File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
-# end
+FILE_PATH = 'public/memos.json'
+
+def get_memos(file_path)
+  File.open(file_path) { |item| JSON.parse(item.read) }
+end
+
+def set_memos(file_path, memos)
+  File.open(file_path, 'w') { |item| JSON.dump(memos, item) }
+end
 
 get '/' do
 	redirect '/memos'
@@ -20,14 +27,31 @@ end
 
 get '/memos/:id' do
 	memos = get_memos(FILE_PATH)
-	@title = memos[parms[:id]]['title']
-	@content = memos[parms[:id]]['content']
+	@title = memos[params[:id]]['title']
+	@content = memos[params[:id]]['content']
 	erb :memo
 end
 
-get '/memos/memo/edit' do
+post '/memos' do
+	title = params[:title]
+	content = params[:content]
+
+	memos = get_memos(FILE_PATH)
+	id = (memos.keys.map(&:to_i).max + 1).to_s
+	memos[id] = { 'title' => title, 'content' => content}
+	set_memos(FILE_PATH, memos)
+
+	redirect '/memos'
+end
+
+get '/memos/:id/edit' do
+	memos = get_memos(FILE_PATH)
+	@title = memos[params[:id]]['title']
+	@content = memos[params[:id]]['content']
 	erb :edit
 end
+
+
 
 # get '/erb_template_page' do
 # 	erb :erb_template_page
