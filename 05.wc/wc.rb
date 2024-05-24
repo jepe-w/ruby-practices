0b7f2eebc8d1cf171fc2ebfc1a1f0ebc66585aff
jepe-w <yoshino_k@vitallead.co.jp>
2024-05-24 11:23:51 +0900

    display_valuesメソッドの改善。optionをハッシュのまま使用する方法に変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,64 +5,77 @@ require 'optparse'
 
 def main
   options = ARGV.getopts('lwc')
+  if options.values.uniq.length == 1
+    options = options.each_key do |key|
+      options[key] = true
+    end
+  end
   filenames = ARGV
-  use_opts = options.select { |_key, value| value }.keys
-  use_opts = %w[l w c] if use_opts.empty?
 
   if filenames.any?
-    display_values = display_files_values(filenames, use_opts)
-    display_values.each { |value| use_opts.length >= 2 ? (puts value.join(' ')) : (puts value) }
+    handle_multiple_files(filenames, options)
   else
-    gets_inputted_values = readlines
-    input_text = gets_inputted_values.join('')
-    display_values = display_files_values(input_text, use_opts)
-    puts display_values.join(' ')
+    inputted_values = readlines
+    input_text = inputted_values.join('')
+    handle_standard_input(input_text, options)
   end
 end
 
-def display_files_values(files, use_opts)
-  if files.instance_of?(String)
-    handle_standard_input(files, use_opts)
-  else
-    handle_multiple_files(files, use_opts)
+def handle_standard_input(input_text, options)
+  detail_hash = create_detail_hash(input_text)
+  display_values([detail_hash], options)
+end
+
+def handle_multiple_files(files, options)
+  file_texts = files.map do |file|
+    File.read(file)
   end
-end
-
-def handle_standard_input(files, use_opts)
-  file_details = files
-  detail_hash = create_detail_hash(file_details, 'input')
-  create_display_values(detail_hash, use_opts)
-end
-
-def handle_multiple_files(files, use_opts)
-  file_details = files.map { |file| File.read(file) }
   if files.length >= 2
-    file_details.push(file_details.join(''))
+    file_texts.push(file_texts.join(''))
     files.push('total')
   end
-  detail_hash = file_details.map.with_index { |filedetail, index| create_detail_hash(filedetail, files[index]) }
-  words_width = detail_hash.map { |hash| hash.select { |_key, value| value.instance_of?(Integer) }.values.max }.max.to_s.length
-  words_width = 0 if detail_hash.length <= 1 && use_opts.length <= 1
-  create_display_values(detail_hash, use_opts, words_width)
+  detail_hash = file_texts.map.with_index do |file_text, index|
+    create_detail_hash(file_text, files[index])
+  end
+  words_width = create_words_width(detail_hash, options)
+  display_values(detail_hash, options, words_width)
 end
 
-def create_detail_hash(filedetails, filename)
+def create_words_width(detail_hash, options)
+  numbers_hash = detail_hash.map do |hash|
+    hash.reject do |key, _value|
+      key == 'name'
+    end
+  end
+
+  if detail_hash.length <= 1 && options.count do |_key, value|
+    value == true
+  end <= 1
+    0
+  else
+    numbers_hash.map do |number|
+      number.values.max
+    end.max.to_s.length
+  end
+end
+
+def create_detail_hash(text, filename = 'default')
   {
     'name' => filename,
-    'c' => filedetails.bytesize,
-    'l' => filedetails.lines.length,
-    'w' => filedetails.split.count
+    'c' => text.bytesize,
+    'l' => text.lines.length,
+    'w' => text.split.count
   }
 end
 
-def create_display_values(detail_hash, use_opts, words_width = 0)
-  standard_input_data = detail_hash.instance_of?(Hash)
-  if standard_input_data
-    use_opts.map { |option| detail_hash[option.to_s] }
-  elsif use_opts.length >= 2
-    detail_hash.map { |hash| use_opts.map { |option| hash[option.to_s].to_s.rjust(words_width) }.push(hash['name']) }
-  else
-    detail_hash.map { |hash| "#{hash[use_opts[0].to_s].to_s.rjust(words_width)} #{hash['name']}" }
+def display_values(file_details, options, words_width = 0)
+  file_details.each do |file_detail|
+    columns = []
+    columns << file_detail['l'].to_s.rjust(words_width) if options['l']
+    columns << file_detail['w'].to_s.rjust(words_width) if options['w']
+    columns << file_detail['c'].to_s.rjust(words_width) if options['c']
+    columns << file_detail['name'] if file_detail['name'] != 'default'
+    puts columns.join(' ')
   end
 end
 
